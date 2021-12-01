133d72236bd83292aebdfdb577261d73d347cca1
David Casanellas <davidcasanellas@dorna.com>
2021-12-01 10:37:19 +0100

    feat: add day1 2021 exercise

---
+++ 2021/day1/index.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+fs.readFile("./input.txt", (error, data) => {
+  if (error) {
+    throw error;
+  }
+
+  const input = data.toString().split("\n");
+
+  const initialValue = 0;
+
+  const reducer = (accumulator, item, index, array) =>
+    index + 1 < array.length && parseInt(item) < array[index + 1]
+      ? accumulator + 1
+      : accumulator;
+
+  const increased = input.reduce(reducer, initialValue);
+  console.log("Total increased:", increased);
+});
